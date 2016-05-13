17ba647196cf09fb114226faaeb93b2fc6d409bf
Caio <caliumi.f@diffusionetessile.it>
2016-05-13 18:54:00 +0200

    WIP

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,41 @@
 from django.db import models
+import pprint
 
-# Create your models here.
+# -------------------------------------------------------------------- #
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	
+	def __str__(self):
+		s = []
+		
+		d = self.__dict__
+		for k in tuple(d.keys()):
+			if k.startswith('_'):
+				d.pop(k)
+		
+		s += [ '%s:' % self.__class__.__name__ ]
+		s += [ pprint.pformat(d, indent=2, compact=False) ]
+		return '\n'.join(s)
+	
+# -------------------------------------------------------------------- #
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	
+	def __str__(self):
+		s = []
+		
+		d = self.__dict__
+		for k in tuple(d.keys()):
+			if k.startswith('_'):
+				d.pop(k)
+		
+		s += [ '%s:' % self.__class__.__name__ ]
+		s += [ pprint.pformat(d, indent=2, compact=False) ]
+		return '\n'.join(s)
+	
+# -------------------------------------------------------------------- #
---
+++ test.py
@@ -0,0 +1,6 @@
+
+from polls.models import Question, Choice
+from django.utils import timezone
+
+print()
+print(Question.objects.all()[0])
